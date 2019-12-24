6186d73f0011eac2fc9c2485b1e650617307f35a
azul915 <k.aoyagi940516@gmail.com>
2019-12-25 04:20:43 +0900

    Add RemoveDuplicateFromSortedList.java

---
+++ LinkedList/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,19 @@
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+class Solution {
+    fun deleteDuplicates(head: ListNode?): ListNode? {
+
+        val set = emptySet<Int>()
+        while (head != null) {
+            set.add(head)
+        }
+        return head
+    }
+}
